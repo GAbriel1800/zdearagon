4216b8b52e0c3c66ae380ef161915a86cf83719f
Thegabri_ <113857452+GAbriel1800@users.noreply.github.com>
2024-09-17 19:17:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
+    // Datos de inicio de sesión correctos
+    const correctUsername = 'GabrielRey';  // Cambia esto por el nombre de usuario correcto
+    const correctPassword = 'miPassword123';  // Cambia esto por la contraseña correcta
+    
+    // Verificar si coinciden
+    if (username === correctUsername && password === correctPassword) {
+        // Si los datos son correctos, redirigir a tareas.html
+        window.location.href = "tareas.html";
+    } else {
+        // Si los datos son incorrectos, mostrar un mensaje de error
+        alert('Usuario o contraseña incorrectos. Inténtalo de nuevo.');
+    }
+});
